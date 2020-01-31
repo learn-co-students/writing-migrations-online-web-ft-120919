2f74b815d879058b954aa2d7d273229738091b50
DevonJamesWright <devonjamess.wright@gmail.com>
2020-01-31 12:55:19 -0500

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,14 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
+    def uo
+    end 
+    def change
+        create_table :students do |t|
+            t.string :name
+        t.string :genre
+        t.integer :age
+        t.string :hometown
+        end 
+    end 
+    def down 
+    end 
 end
